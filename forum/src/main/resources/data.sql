38fb718207e406f190665e4ff7c8a160fde011f0
Silas Felipe Garcia <silas.garcia@mfconsulting.com.br>
2019-07-04 22:02:00 -0300

    começando com spring data jpa

---
+++ forum/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'alunoe@email.com', '123456');
+
+INSERT INTO CURSO(nome, categoria) VALUES('Spring boot', 'Programação');
+INSERT INTO CURSO(nome, categoria) VALUES('Html5', 'Front-end');
+
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida', 'Erro ao criar projeto', '2019-05-05 20:33', 'NAO_RESPONDIDO', 1, 1);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida', 'Erro ao criar projeto', '2019-05-05 20:33', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida', 'Erro ao criar projeto', '2019-05-05 20:33', 'NAO_RESPONDIDO', 1, 1);
